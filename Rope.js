3917ecb1645d9bfb2fdf7bec41c1be258df6b524
Sab3rson <67917394+Sab3rson@users.noreply.github.com>
2020-11-29 12:06:42 -0600

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,110 @@
+class Rope {
+
+    constructor(bodyA,pointB){
+
+        var options={ 
+          bodyA:bodyA,
+          pointB:pointB,
+          stiffness:1.2,
+          length:250
+        }
+
+        this.rope=Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world,this.rope);
+  }
+    display(){
+
+        var pointA=this.rope.bodyA.position;
+        var pointB=this.pointB;
+
+      push();
+        strokeWeight(4);
+        stroke("gray");
+        fill(20,20,20);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        
+      pop();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Rope {
+//   constructor(bodyA, pointB) {
+//     var options = {
+//       bodyA: bodyA,
+//       pointB: pointB,
+//       stiffness: 1.2,
+//       length: 250,
+//     };
+
+//     this.pointB = pointB;
+//     this.rope = Constraint.create(options);
+//     World.add(world, this.rope);
+//   }
+//   attach(body) {
+//     this.rope.bodyA = body;
+//   }
+
+//   fly() {
+//     this.rope.bodyA = null;
+//   }
+
+//   display() {
+//     if (this.rope.bodyA) {
+//       var pointA = this.rope.bodyA.position;
+//       var pointB = this.pointB;
+//       push();
+
+//       stroke(48, 22, 8);
+//       strokeWeight(3);
+
+//       line(pointB.x, pointB.y, pointA.x, pointA.y);
+
+//       pop();
+//     }
+//   }
+// }
